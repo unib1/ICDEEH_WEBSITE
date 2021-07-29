fbdc92652ae7b0abf7fead9c0485b7223c0060c6
Uni_b <73627672+unib1@users.noreply.github.com>
2021-07-29 02:09:43 +0100

    Add files via upload

--- form-action.php
+++ form-action.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['send'])) {
-    $to_email = 'nfontatah @ company . com';
+    $to_email = 'emmanuel4unib@gmail.com';
     $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
